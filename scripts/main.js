4be8556b3dcde83996183119a4b202331695a7a3
opti-mus <71773616+opti-mus@users.noreply.github.com>
2020-10-05 14:18:15 +0300

    add sign and menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,50 @@
 const tabsOut = document.querySelectorAll('.tab-out');
 const tabs = document.querySelectorAll('.tab');
 
-Tabs(tabsOut,tabs);
+Tabs(tabsOut, tabs);
+
+document.addEventListener('click', (e) => {
+    const menu = document.querySelector('.header-menu')
+    const bars = document.querySelector('.header-bars');
+    const signIn = document.querySelector('.sign-in');
+    const signUp = document.querySelector('.sign-up');
+    const signWrapper = document.querySelector('.sign-wrapper');
+
+    if (e.target.dataset.menu) {
+
+        bars.classList.toggle('open');
+        menu.classList.toggle('open');
+        menu.querySelectorAll('.sign').forEach(el => {
+            el.onclick = function (e) {
+                switch (this.dataset.sign) {
+                    case 'in':
+                        e.preventDefault();
+                        menu.classList.remove('open');
+                        bars.classList.remove('open');
+                        signWrapper.classList.add('in');
+                        signWrapper.classList.remove('up');
+                        break;
+                    case 'up':
+                        e.preventDefault();
+                        menu.classList.remove('open');
+                        bars.classList.remove('open');
+                        signWrapper.classList.add('up');
+                        signWrapper.classList.remove('in');
+                        break;
+                }
+            }
+
+        })
+
+    } else if (e.target.classList.contains('sign-wrapper')) {
+        e.target.classList.remove('up')
+        e.target.classList.remove('in')
+    }
+})
+
+function closeMenu(e, menu, signWrap, sign) {
+    e.preventDefault();
+    menu.classList.remove('open');
+    signWrap.style.display = 'block';
+    sign.style.display = 'block';
+}
